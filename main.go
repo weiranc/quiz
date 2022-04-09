746d8eb3cd9b7da0f01df2eb5ed39b6e98a6c8ab
Weiran (Rebecca) Cheng <91348196+weiranc@users.noreply.github.com>
2022-04-09 11:49:53 -0700

    Read the csv file line by line

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	for {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%+v\n", rec)
+
+	}
+
+}
